a79be6d18feb58abd7595088abe962778116660a
Brian H Suarez <bhsuarez@ZorroMacBook-WLAN.zorro.local>
2020-11-13 23:37:39 -0600

    added mySQL query

--- jukebox.py
+++ jukebox.py
@@ -1,7 +1,27 @@
 import mysql.connector
 
-db_connection = mysql.connector.connect(host="localhost",
-                                        user="root",
-                                        passwd="rocky.123",
-                                        auth_plugin='mysql_native_password')
-print(db_connection)
+# Sets up the database connection
+databoi = mysql.connector.connect(host="localhost",
+                                  user="root",
+                                  passwd="rocky.123",
+                                  database="vinyls",
+                                  auth_plugin='mysql_native_password')
+cursor = databoi.cursor()
+
+# Prints the db connection
+print("Databoi CONNECTED " + "'" + str(databoi) + "'")
+
+# Define album
+artist = "Kanye West"
+
+# Query
+query = ("SELECT * FROM albums "
+         "WHERE artist_name = "+'"'+artist+'"'+";")
+
+cursor.execute(query)
+
+print(query)
+
+# Closes the db connection
+cursor.close()
+databoi.close()
