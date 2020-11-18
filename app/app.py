d91c1209f5aab0cd9ebce8b330994c5a1d059317
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2020-11-18 12:57:21 -0600

    dockerfile update

--- app/app.py
+++ app/app.py
@@ -5,25 +5,6 @@ import json
 
 app = Flask(__name__)
 
-def get_albums() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'vinyls'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM albums')
-    results = [{artist_name: title+"<br>"}
-               for (id, title, artist_name, artist_id, discogs_id, barcode)
-               in cursor]
-    cursor.close()
-    connection.close()
-
-    return results
-
 
 @app.route('/')
 def index():
