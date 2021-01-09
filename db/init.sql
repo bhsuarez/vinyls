607ccec4f0f1f6fc06dc233e7574c259bfadd117
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2021-01-08 21:39:30 -0600

    Removed docker stuff

--- db/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.albums (
-	"﻿id" int4 NULL,
-	title varchar(1024) NULL,
-	artist_name varchar(1024) NULL,
-	artist_id int4 NULL,
-	discogs_id int4 NULL,
-	barcode varchar(1024) NULL
-);
