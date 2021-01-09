d555ac3ba93be8a0b8b2af97ba712a4173dfffc5
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2021-01-09 11:03:27 -0600

    Fucking works again -- gosh
    Fixed docker and now it finally serves the page

--- app/postgresconnector.py
+++ app/postgresconnector.py
@@ -4,7 +4,8 @@ import psycopg2
 databoi = psycopg2.connect(dbname="vinyls",
                            user="root",
                            password="root",
-                           host="0.0.0.0")
+                           host="0.0.0.0",
+                           port="5432")
 
 
 def connect():
