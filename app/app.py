b548661e85c8b94e656541eb3c0689091ced480c
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2021-01-10 16:47:30 -0600

    SQL statements work!
    I was able to get the db to work!

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine("postgresql://root:root@db:5432/vinyls")
+db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
+
+app.secret_key = '12345678' # this key is used to communicate with database.
+#   Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 
 @app.route('/')
@@ -9,21 +18,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/albumlist')
-def albumlist():
-    # try:
-    #     cursor = psycopg2.connect(dbname="vinyls",
-    #                               user="root",
-    #                               password="root",
-    #                               host="0.0.0.0",
-    #                               port="5432")
-    # except:
-    #     print("can't connect to database")
-    # cursor.execute()
-    # data = cursor.fetchall()
-    return render_template("albumlist.html"
-                           #, data=data
-                           )
+@app.route('/albumlist', methods=['GET'])
+def album_list():
+    albums = db.execute("SELECT * FROM public.albums")
+    return render_template("albumlist.html", albums=albums)
 
 
 if __name__ == '__main__':
