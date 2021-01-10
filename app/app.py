d120fdcd46529abf6c269067ad28fdc077412f30
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2021-01-09 20:16:48 -0600

    both pages working

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import psycopg2
 
 app = Flask(__name__)
 
@@ -7,6 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/albumlist')
+def albumlist():
+    # try:
+    #     cursor = psycopg2.connect(dbname="vinyls",
+    #                               user="root",
+    #                               password="root",
+    #                               host="0.0.0.0",
+    #                               port="5432")
+    # except:
+    #     print("can't connect to database")
+    # cursor.execute()
+    # data = cursor.fetchall()
+    return render_template("albumlist.html"
+                           #, data=data
+                           )
 
 
 if __name__ == '__main__':
