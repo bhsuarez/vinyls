85d68772e3c4a3d2fe0fa92a9a7c9bcde8bddd27
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2022-03-14 22:36:48 -0500

    Update telegram_worker.py

--- app/telegram_worker.py
+++ app/telegram_worker.py
@@ -1,7 +1,7 @@
 import telegram
 import os
 import logging
-
+import requests
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
@@ -9,4 +9,8 @@ bot = telegram.Bot(token=os.getenv('telegram_token'))
 
 
 def send_telegram_message(ip_address):
-    bot.send_message(text=f'There was a visit from {ip_address}', chat_id=os.getenv('bhsuarez_chat_id'))
+    requestz = "http://ip-api.com/json/" + ip_address
+    response = requests.get(requestz)
+    result = response.content.decode()
+    bot.send_message(text=f'There was a visit from {ip_address}'
+                          f'{result}', chat_id=os.getenv('bhsuarez_chat_id'))
