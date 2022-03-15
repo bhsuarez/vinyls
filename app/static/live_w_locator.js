8d9a964667af338770c65cd78e02bfb70715b5f8
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2022-03-14 19:23:31 -0500

    Update live_w_locator.js
    removed image for onDetect

--- app/static/live_w_locator.js
+++ app/static/live_w_locator.js
@@ -289,7 +289,7 @@ $(function() {
            var $node = null, canvas = Quagga.canvas.dom.image;
 
            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-           $node.find("img").attr("src", canvas.toDataURL());
+           //$node.find("img").attr("src", canvas.toDataURL());
            $node.find("h4.code").html(code);
            $("#result_strip ul.thumbnails").prepend($node);
        }
